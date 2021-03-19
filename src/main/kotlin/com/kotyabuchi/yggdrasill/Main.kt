4173ef69095170cef8cc549ee09172bca37d11ad
kotyabuchi <kotyabuchi@gmail.com>
2021-03-20 00:56:25 +0900

    Add: PaperPluginのベースを追加

---
+++ src/main/kotlin/com/kotyabuchi/yggdrasill/Main.kt
@@ -0,0 +1,14 @@
+package com.kotyabuchi.yggdrasill
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Main : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
