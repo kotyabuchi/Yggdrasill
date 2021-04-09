e01127a78b68357898b53a806e123d9bb058367b
kotyabuchi <kotyabuchi@gmail.com>
2021-04-10 01:12:43 +0900

    Add: Koinを導入

--- src/main/kotlin/com/kotyabuchi/yggdrasill/Main.kt
+++ src/main/kotlin/com/kotyabuchi/yggdrasill/Main.kt
@@ -1,14 +1,27 @@
 package com.kotyabuchi.yggdrasill
 
 import org.bukkit.plugin.java.JavaPlugin
+import org.koin.core.context.startKoin
+import org.koin.dsl.module
 
 class Main : JavaPlugin() {
 
     override fun onEnable() {
         // Plugin startup logic
+        setupKoin()
     }
 
     override fun onDisable() {
         // Plugin shutdown logic
+    }
+
+    private val pluginModule = module {
+        single { this@Main }
+    }
+
+    private fun setupKoin() {
+        startKoin {
+            modules(listOf(pluginModule))
+        }
     }
 }
